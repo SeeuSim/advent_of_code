8505ec4c5b9e5750e81c938a8705ac391f1a5a7d
SeeuSim <seeusimong@gmail.com>
2023-12-18 09:52:35 +0800

    Add working soln for d10 p2
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- 2023/src/day_10/part_2.rs
+++ 2023/src/day_10/part_2.rs
@@ -1,9 +1,6 @@
 use crate::utils::extract_file;
 
-use std::{
-    collections::HashSet,
-    io::BufRead,
-};
+use std::{collections::HashSet, collections::VecDeque, io::BufRead};
 
 #[derive(Debug, Clone, Copy)]
 enum Dir {
@@ -13,7 +10,194 @@ enum Dir {
     Right,
 }
 
+/**
+ * Attempt at replicating success from other soln, but gives wrong vals
+ */
 pub fn pipe_maze_two(file_name: &String) {
+    let reader = extract_file(file_name).expect("An error occurred while reading the file");
+
+    let mut s_loc = (usize::MAX, usize::MAX);
+    let mut maze = reader
+        .lines()
+        .filter_map(std::result::Result::ok)
+        .enumerate()
+        .map(|(lnum, line)| {
+            let out = line.chars();
+            let pos = out.clone().position(|c| c == 'S');
+            if let Some(col) = pos {
+                s_loc = (lnum, col);
+            }
+            out.collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+
+    let mut possible_s_vals = HashSet::from(['|', '-', 'J', 'L', '7', 'F']);
+
+    let mut visited = HashSet::from([s_loc]);
+
+    let mut queue = VecDeque::from([s_loc]);
+
+    while queue.len() > 0 {
+        let (r, c) = match queue.pop_front() {
+            None => break,
+            Some(val) => val,
+        };
+
+        let curr_pipe = maze[r][c];
+
+        if r > 0
+            && matches!(curr_pipe, 'S' | '|' | 'J' | 'L')
+            && matches!(maze[r - 1][c], '|' | '7' | 'F')
+            && !visited.contains(&(r - 1, c))
+        {
+            visited.insert((r - 1, c));
+            queue.push_back((r - 1, c));
+            if curr_pipe == 'S' {
+                possible_s_vals = HashSet::from_iter(
+                    possible_s_vals
+                        .intersection(&HashSet::from(['|', 'J', 'L']))
+                        .map(|&v| v),
+                );
+            }
+        }
+
+        if r < maze.len() - 1
+            && matches!(curr_pipe, 'S' | '|' | '7' | 'F')
+            && matches!(maze[r + 1][c], '|' | 'J' | 'L')
+            && !visited.contains(&(r + 1, c))
+        {
+            visited.insert((r + 1, c));
+            queue.push_back((r + 1, c));
+            if curr_pipe == 'S' {
+                possible_s_vals = HashSet::from_iter(
+                    possible_s_vals
+                        .intersection(&HashSet::from(['|', '7', 'F']))
+                        .map(|&v| v),
+                );
+            }
+        }
+
+        if c > 0
+            && matches!(curr_pipe, 'S' | '-' | 'J' | '7')
+            && matches!(maze[r][c - 1], '-' | 'L' | 'F')
+            && !visited.contains(&(r, c - 1))
+        {
+            visited.insert((r, c - 1));
+            queue.push_back((r, c - 1));
+            if curr_pipe == 'S' {
+                possible_s_vals = HashSet::from_iter(
+                    possible_s_vals
+                        .intersection(&HashSet::from(['-', 'J', '7']))
+                        .map(|&v| v),
+                );
+            }
+        }
+
+        if c < maze[0].len() - 1
+            && matches!(curr_pipe, 'S' | '-' | 'L' | 'F')
+            && matches!(maze[r][c + 1], '-' | 'J' | '7')
+            && !visited.contains(&(r, c + 1))
+        {
+            visited.insert((r, c + 1));
+            queue.push_back((r, c + 1));
+            if curr_pipe == 'S' {
+                possible_s_vals = HashSet::from_iter(
+                    possible_s_vals
+                        .intersection(&HashSet::from(['-', 'L', 'F']))
+                        .map(|&v| v),
+                );
+            }
+        }
+    }
+
+    // There should only be 1 S Value
+    let s = match possible_s_vals.len() {
+        1 => possible_s_vals.into_iter().next().unwrap(),
+        _ => {
+            eprintln!(
+                "Supposed to have only 1 possible S value, but encountered this: {:?}",
+                possible_s_vals
+            );
+            return;
+        }
+    };
+
+    maze[s_loc.0][s_loc.1] = s;
+
+    for r in 0..maze.len() {
+        for c in 0..maze.len() {
+            if !visited.contains(&(r, c)) {
+                maze[r][c] = '.';
+            }
+        }
+    }
+
+    let mut outside_values: HashSet<(usize, usize)> = HashSet::new();
+
+    for r in 0..maze.len() {
+        let mut within = false;
+        let mut up: Option<bool> = None;
+        for c in 0..maze[0].len() {
+            let chr = maze[r][c];
+            match chr {
+                '|' => {
+                    if !up.is_none() {
+                        eprintln!("Should be None: {}, {}, [{}]", r, c, chr);
+                        std::process::exit(1);
+                    }
+                    within = !within;
+                }
+                '-' => {
+                    if !up.is_some() {
+                        eprintln!("Should be Some: {}, {}, [{}]", r, c, chr);
+                        std::process::exit(1);
+                    }
+                }
+                'L' | 'F' => {
+                    if !up.is_none() {
+                        eprintln!("Should be None: {}, {}, [{}]", r, c, chr);
+                        std::process::exit(1);
+                    }
+                    up = Some(chr == 'L');
+                }
+                '7' | 'J' => {
+                    if !up.is_some() {
+                        eprintln!("Should be Some: {}, {}, [{}]", r, c, chr);
+                        std::process::exit(1);
+                    }
+                    match up {
+                        Some(v) => {
+                            if (v && chr != 'J') || (!v && chr != '7') {
+                                within = !within;
+                            }
+                        }
+                        _ => {
+                            if chr != '7' {
+                                within = !within;
+                            }
+                        }
+                    }
+                    up = None;
+                }
+                '.' => {}
+                _ => unreachable!(),
+            }
+            if !within {
+                outside_values.insert((r, c));
+            }
+        }
+    }
+
+    println!(
+        "Answer: {}",
+        maze.len() * maze[0].len() - outside_values.union(&visited).count()
+    );
+}
+
+/**
+ * Broken solution, overcounts by 6
+ */
+pub fn og_pipe_maze_two(file_name: &String) {
     let reader = extract_file(file_name).expect("An error occurred while reading the file");
 
     let mut s_loc = (usize::MAX, usize::MAX);
