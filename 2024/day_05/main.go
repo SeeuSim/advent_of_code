016aac54c70c8622a8ab0a8cbc0f52ca1b33ba4a
SeeuSim <seeusimong@gmail.com>
2024-12-16 08:05:48 +0800

    Add day 5 p 2 soln
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- 2024/day_05/main.go
+++ 2024/day_05/main.go
@@ -49,7 +49,42 @@ func RunP1() {
 }
 
 func RunP2() {
+	f := utils.OpenFile(5, false)
+	defer f.Close()
 
+	// Given X | Y, X has to appear before Y
+	rules, updates := GetData(f)
+	sum := 0
+	for _i := range updates {
+		update := updates[_i]
+		isCorrect := true
+		for i := 1; i < len(update); i++ {
+			if !isCorrect {
+				break
+			}
+			curr := update[i]
+			for j := 0; j < i; j++ {
+				// For each prior element, check if i-th element is supposed to appear before
+				if !isCorrect {
+					break
+				}
+				prior := update[j]
+				priorRules := rules[prior]
+				for ruleIdx := range priorRules {
+					if priorRules[ruleIdx] == curr {
+						// illegal
+						isCorrect = false
+						break
+					}
+				}
+			}
+		}
+		if !isCorrect {
+			correctedUpdate := TopoSort(update, rules)
+			sum += correctedUpdate[len(correctedUpdate)/2]
+		}
+	}
+	fmt.Printf("Sum: %d\n", sum)
 }
 
 func GetData(f *os.File) (map[int][]int, [][]int) {
@@ -84,4 +119,53 @@ func GetData(f *os.File) (map[int][]int, [][]int) {
 		}
 	}
 	return rules, updates
+}
+
+func TopoSort(incorrectUpdate []int, rules map[int][]int) []int {
+	var out []int
+	var queue []int
+	graph := make(map[int][]int)
+	seen := make(map[int]bool)
+
+	for _, value := range incorrectUpdate {
+		seen[value] = true
+	}
+
+	// TODO: Build graph using all relevant rules from row
+	for _, value := range incorrectUpdate {
+		rule := rules[value]
+		var newRule []int
+		for _, v := range rule {
+			if seen[v] {
+				newRule = append(newRule, v)
+			}
+		}
+		graph[value] = newRule
+
+		if len(newRule) == 0 {
+			queue = append(queue, value)
+		}
+	}
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+		out = append(out, curr)
+		for key, beforeEdges := range graph {
+			idx := -1
+			for i, v := range beforeEdges {
+				if v == curr {
+					idx = i
+					break
+				}
+			}
+			if idx < 0 {
+				continue
+			}
+			graph[key] = append(beforeEdges[:idx], beforeEdges[idx+1:]...)
+			if len(graph[key]) == 0 {
+				queue = append(queue, key)
+			}
+		}
+	}
+	return out
 }
