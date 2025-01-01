620457c40e439d641c91ca3d9ddb0460111151bc
SeeuSim <seeusimong@gmail.com>
2025-01-01 13:49:57 +0900

    Add d17 p2 soln
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- 2024/day_17/main.go
+++ 2024/day_17/main.go
@@ -4,7 +4,6 @@ import (
 	"2024/utils"
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"regexp"
 	"strconv"
@@ -14,60 +13,95 @@ import (
 func RunP1() {
 	f := utils.OpenFile(17, false)
 	g := GetGame(f)
-	instructions := g.instructions
-	currInst := 0
-	output := []string{}
-	for currInst < len(instructions) {
-		curr := instructions[currInst]
-		switch curr.opcode {
-		case adv:
-			g.a = g.a / int(math.Pow(float64(2), float64(GetComboOperand(curr.operand, g))))
-		case bxl:
-			g.b = g.b ^ curr.operand
-		case bst:
-			g.b = GetComboOperand(curr.operand, g) % 8
-		case jnz:
-			if g.a != 0 {
-				currInst = curr.operand / 2
-				continue
+	a, b, c := g.a, 0, 0
+	outputS := []string{}
+	iPtr := 0
+	for iPtr < len(g.instructions) {
+		comboOp := GetComboOperand(g.instructions[iPtr+1], Game{a: a, b: b, c: c})
+
+		opCode := g.instructions[iPtr]
+		if opCode == 0 {
+			a >>= comboOp
+		} else if opCode == 1 {
+			b ^= g.instructions[iPtr+1]
+		} else if opCode == 2 {
+			b = comboOp & 7
+		} else if opCode == 3 {
+			if a != 0 {
+				iPtr = g.instructions[iPtr+1] - 2
 			}
-		case bxc:
-			g.b = g.b ^ g.c
-		case out:
-			output = append(output, strconv.Itoa(GetComboOperand(curr.operand, g)%8))
-		case bdv:
-			g.b = g.a / int(math.Pow(float64(2), float64(GetComboOperand(curr.operand, g))))
-		case cdv:
-			g.c = g.a / int(math.Pow(float64(2), float64(GetComboOperand(curr.operand, g))))
+		} else if opCode == 4 {
+			b ^= c
+		} else if opCode == 5 {
+			outputS = append(outputS, strconv.Itoa(comboOp%8))
+		} else if opCode == 6 {
+			b = a >> comboOp
+		} else if opCode == 7 {
+			c = a >> comboOp
 		}
-		currInst++
+		iPtr += 2
 	}
-	fmt.Println(strings.Join(output, ","))
+	fmt.Println(strings.Join(outputS, ","))
 }
 
 func RunP2() {
-	// TODO: Implement Part 2
+	f := utils.OpenFile(17, false)
+	game := GetGame(f)
+
+	instructions := game.instructions
+
+	Solve(len(instructions)-1, 0, instructions)
+}
+
+func Solve(idx, A int, game []int) bool {
+	if idx < 0 {
+		fmt.Println(A)
+		return true
+	}
+
+	for d := 0; d < 8; d++ {
+		a, b, c := (A<<3)|d, 0, 0
+		iPtr := 0
+		output := 0
+		for iPtr < len(game) {
+			comboOp := GetComboOperand(game[iPtr+1], Game{a: a, b: b, c: c})
+
+			opCode := game[iPtr]
+			if opCode == 0 {
+				a >>= comboOp
+			} else if opCode == 1 {
+				b ^= game[iPtr+1]
+			} else if opCode == 2 {
+				b = comboOp & 7
+			} else if opCode == 3 {
+				if a != 0 {
+					iPtr = game[iPtr+1] - 2
+				}
+			} else if opCode == 4 {
+				b ^= c
+			} else if opCode == 5 {
+				output = comboOp % 8
+				break
+			} else if opCode == 6 {
+				b = a >> comboOp
+			} else if opCode == 7 {
+				c = a >> comboOp
+			}
+			iPtr += 2
+		}
+
+		if output == game[idx] && Solve(idx-1, (A<<3)|d, game) {
+			return true
+		}
+	}
+
+	return false
 }
 
 type Game struct {
 	a, b, c      int
-	instructions []Instruction
+	instructions []int
 }
-
-type Instruction struct {
-	opcode, operand int
-}
-
-const (
-	adv = 0
-	bxl = 1
-	bst = 2
-	jnz = 3
-	bxc = 4
-	out = 5
-	bdv = 6
-	cdv = 7
-)
 
 func GetComboOperand(operand int, g Game) int {
 	switch operand {
@@ -96,16 +130,9 @@ func GetGame(f *os.File) Game {
 		l++
 		if l >= 3 {
 			parts := strings.Split(line[9:], ",")
-			var inst Instruction
-			for i, p := range parts {
+			for _, p := range parts {
 				p, _ := strconv.Atoi(p)
-				if i%2 == 0 {
-					inst.opcode = p
-				} else {
-					inst.operand = p
-					out.instructions = append(out.instructions, inst)
-					inst = Instruction{}
-				}
+				out.instructions = append(out.instructions, p)
 			}
 		}
 		dReg := regexp.MustCompile(`\d+`)
