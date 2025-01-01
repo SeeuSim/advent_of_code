31a076497e5ff634cbdb606aafc252acddf2b383
SeeuSim <seeusimong@gmail.com>
2025-01-01 18:27:52 +0900

    Add d20 p2
    Signed-off-by: SeeuSim <seeusimong@gmail.com>

--- 2024/day_20/main.go
+++ 2024/day_20/main.go
@@ -4,7 +4,9 @@ import (
 	"2024/utils"
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"sort"
 )
 
 func RunP1() {
@@ -56,7 +58,58 @@ func RunP1() {
 }
 
 func RunP2() {
-	// TODO: Implement Part 2
+	f := utils.OpenFile(20, false)
+	maze, s, e := GetGame(f)
+	X, Y := len(maze[0]), len(maze)
+
+	deltas := []Coord{
+		{0, -1}, {1, 0}, {0, 1}, {-1, 0},
+	}
+
+	queue := []QueueNode{{s, 0, make(map[Coord]int)}}
+
+	var visited map[Coord]int
+	for len(queue) > 0 {
+		curr := queue[0]
+		queue = queue[1:]
+		coord, time, seen := curr.coord, curr.time, curr.seen
+		seen[coord] = time
+
+		if coord == e {
+			visited = CopyMap(seen)
+			fmt.Println("End Reached", len(visited))
+			break
+		}
+
+		for _, d := range deltas {
+			nC := Coord{coord.x + d.x, coord.y + d.y}
+			_, s := seen[nC]
+			if nC.x >= 0 && nC.x < X && nC.y >= 0 && nC.y < Y && !s && maze[nC.y][nC.x] != '#' {
+				queue = append(queue, QueueNode{nC, time + 1, CopyMap(seen)})
+			}
+		}
+	}
+
+	sortedPath := make(PairList, len(visited))
+	i := 0
+	for k, v := range visited {
+		sortedPath[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(sortedPath)
+
+	c := 0
+	for t2 := 100; t2 < len(visited); t2++ {
+		for t1 := 0; t1 < t2-100; t1++ {
+			p1 := sortedPath[t1]
+			p2 := sortedPath[t2]
+			dist := int(math.Abs(float64(p1.coord.x-p2.coord.x))) + int(math.Abs(float64(p1.coord.y-p2.coord.y)))
+			if dist <= 20 && t2-t1-dist >= 100 {
+				c++
+			}
+		}
+	}
+	fmt.Println("Num Cheats:", c)
 }
 
 type Coord struct {
@@ -67,6 +120,25 @@ type QueueNode struct {
 	coord Coord
 	time  int
 	seen  map[Coord]int
+}
+
+type Pair struct {
+	coord Coord
+	time  int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int {
+	return len(p)
+}
+
+func (p PairList) Less(i, j int) bool {
+	return p[i].time < p[j].time
+}
+
+func (p PairList) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
 }
 
 func CopyMap(seen map[Coord]int) map[Coord]int {
